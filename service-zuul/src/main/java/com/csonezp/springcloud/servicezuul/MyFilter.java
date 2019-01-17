22ea55c3f6b084fdf7f3b9b1e5d0966f314b16c9
zhangpeng34 <zhangpeng34@meituan.com>
2019-01-17 14:30:40 +0800

    add zuul filter

---
+++ service-zuul/src/main/java/com/csonezp/springcloud/servicezuul/MyFilter.java
@@ -0,0 +1,55 @@
+// Copyright (C) 2019 Meituan
+// All rights reserved
+package com.csonezp.springcloud.servicezuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zhangpeng34
+ * Created on 2019/1/16 下午9:23
+**/
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        if(StringUtils.isEmpty(token)){
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+                log.error("token is empty",e);
+            }
+            return null;
+        }
+        return null;
+    }
+}
